062e42e5b4d13a081a3fa65ba5c7bcb4508cf01c
nelsonh88 <nelson.huezo88@gmail.com>
2021-08-14 15:24:59 -0400

    Updates JS code - I updated the JS to show the 'nav-links' on desktop if they are in the 'display: none' state from mobile view when they resize their screen from mobile view to desktop view

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 "use strict"
 
+let menu = document.getElementById("nav-links")
 
 const toggle = function () {
-    let menu = document.getElementById("nav-links")
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
+    if (screen.width < 768) {
+        console.log('mobile size');
+        if (menu.style.display === "block") {
+            menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
     }
+
 }
+// adding this line of code just in case someone resizes their window from mobile size to desktop size
+window.addEventListener("resize", function () {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        menu.style.display = "block";
+        console.log("Screen width is at least 768px")
+    } else {
+        console.log("Screen less than 768px")
+    }
+})
 
 // document.getElementById("ham-btn").addEventListener("click", toggle);
 document.getElementById("nav-links").addEventListener("click", toggle);
